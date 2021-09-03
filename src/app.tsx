a137af00b6b5b6b91f1cb6f535f3e948c868124f
akhil-keyvalue <akhil@keyvalue.systems>
2021-09-03 23:38:47 +0530

    defining different environment

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import Logo from './logo.svg';
 
 export const App = () => {
   return <>
-    <h1>Shared Component Library</h1>
+    <h1>Shared Component Library - {process.env.NODE_ENV} - {process.env.name}</h1>
     <img src={Image} alt="react-logo" />
     <img src={Logo} alt="react-logo" width="300" />
   </>
